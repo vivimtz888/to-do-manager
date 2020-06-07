62b48ce758d423db3d02422b6a649cf5c62f5a38
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-07 18:50:47 -0500

    create new task view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new', as: :new
   get 'tasks/:id', to: 'tasks#show', as: :task
+
 end
