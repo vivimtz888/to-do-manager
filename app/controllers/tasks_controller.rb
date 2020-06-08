0975ec97eabf413cbdec2e5d69f35862af7b6689
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-07 20:01:13 -0500

    refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
         @tasks = Task.all
     end
 
     def show
-      @task = Task.find(params[:id])
+     # @task = Task.find(params[:id])
     end
 
     def new
@@ -18,17 +19,17 @@ class TasksController < ApplicationController
     end
 
     def edit
-      @task = Task.find(params[:id])
+     # @task = Task.find(params[:id])
     end
 
     def update
-      @task = Task.find(params[:id])
+     # @task = Task.find(params[:id])
       @task.update(task_params)
-      redirect_to task_path(@task)
+      redirect_to tasks_path
     end
 
     def destroy
-      @task = Task.find(params[:id])
+      #@task = Task.find(params[:id])
       @task.destroy
 
       redirect_to tasks_path
@@ -37,6 +38,10 @@ class TasksController < ApplicationController
 
 
     private
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
 
     def task_params
       params.require(:task).permit(:title, :details, :completed)
