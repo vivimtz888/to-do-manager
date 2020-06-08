0200cb76e732e09163b265d986c03d025f501275
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-07 19:49:29 -0500

    destroy method done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,6 +27,13 @@ class TasksController < ApplicationController
       redirect_to task_path(@task)
     end
 
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+
+      redirect_to tasks_path
+    end
+
 
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
 
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
