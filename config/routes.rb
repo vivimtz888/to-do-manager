b8cc21366d1fd697c4d7a4b79e97e13161730765
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-07 19:24:13 -0500

    edit task done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get 'tasks/new', to: 'tasks#new', as: :new
   get 'tasks/:id', to: 'tasks#show', as: :task
+
   post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
 
 end
